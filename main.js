39d68c674f26f3091dfc7eaa274c646bd2244397
Tolibjon0601 <egamberdiyevtolibjon0601@gmail.com>
2024-05-16 07:55:19 +0500

    end

--- main.js
+++ main.js
@@ -1,35 +1,99 @@
-class Animal {
-	constructor(name) {
-		this.name = name;
+// class Animal {
+// 	constructor(name) {
+// 		this.name = name;
+// 	}
+// 	speak() {
+// 		return "Animal sound";
+// 	}
+// }
+
+// class Dog extends Animal {
+// 	speak() {
+// 		return "Woof!";
+// 	}
+// }
+// class Cat extends Animal {
+// 	speak() {
+// 		return "Meyav!";
+// 	}
+// }
+// class Lion extends Animal {
+// 	speak() {
+// 		return "RRRR!";
+// 	}
+// }
+// class Donkey extends Animal {
+// 	speak() {
+// 		return "IOIOIOIO!";
+// 	}
+// }
+
+// const animals = [new Dog("Simba"), new Cat("Malla"), new Lion("Scar"), new Donkey("Hotik")];
+
+// animals.forEach((animal) => {
+// 	console.log(animal.name + "ning" + " ovozi " + animal.speak());
+// });
+
+
+
+
+// Car nomli sinf
+class Car {
+	constructor(make, model, year) {
+			this.make = make;
+			this.model = model;
+			this.year = year;
 	}
-	speak() {
-		return "Animal sound";
+
+	drive() {
+			console.log("Mashina haydashmoqda.");
+	}
+
+	stop() {
+			console.log("Mashina to'xtatildi.");
 	}
 }
 
-class Dog extends Animal {
-	speak() {
-		return "Woof!";
+class ElectricCar extends Car {
+	constructor(make, model, year, batteryCapacity) {
+			super(make, model, year);
+			this.batteryCapacity = batteryCapacity;
 	}
-}
-class Cat extends Animal {
-	speak() {
-		return "Meyav!";
+
+	drive() {
+			console.log("elektromobil jimgina boshqariladi.");
 	}
-}
-class Lion extends Animal {
-	speak() {
-		return "RRRR!";
-	}
-}
-class Donkey extends Animal {
-	speak() {
-		return "IOIOIOIO!";
+
+	charge() {
+			console.log("elektr mashina zaryadlanmoqda.");
 	}
 }
 
-const animals = [new Dog("Simba"), new Cat("Malla"), new Lion("Scar"), new Donkey("Hotik")];
 
-animals.forEach((animal) => {
-	console.log(animal.name + "ning" + " ovozi " + animal.speak());
-});
+class HybridCar extends Car {
+	constructor(make, model, year, fuelType, batteryCapacity) {
+			super(make, model, year);
+			this.fuelType = fuelType;
+			this.batteryCapacity = batteryCapacity;
+	}
+
+	drive() {
+			console.log("Hibrid mashina samarali haydashmoqda.");
+	}
+
+	charge() {
+			console.log("Gibrid mashina zaryadlanmoqda..");
+	}
+}
+
+function testDrive(car) {
+	car.drive();
+}
+
+const myCar = new Car("Toyota", "Camry", 2024);
+const myElectricCar = new ElectricCar("Tesla", "Model S", 2021, "100 kWh");
+const myHybridCar = new HybridCar("Toyota", "Prius", 2020, "Benzin", "10 kWh");
+
+testDrive(myCar);
+testDrive(myElectricCar);
+testDrive(myHybridCar);
